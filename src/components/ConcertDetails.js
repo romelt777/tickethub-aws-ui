460d056f92509503f4e8992aa0f0dcf464b92893
romelt777 <romel.tan777@gmail.com>
2026-02-17 17:52:15 -0400

    fixed date issue

--- src/components/ConcertDetails.js
+++ src/components/ConcertDetails.js
@@ -2,8 +2,13 @@
 
 import Link from 'next/link';
 import { use, useState } from 'react'
+import { useSearchParams } from 'next/navigation'
+
 
 const ConcertDetails = ({ concert }) => {
+    const searchParams = useSearchParams()
+    const date = searchParams.get('date');
+
     const [quantity, setQuantity] = useState(1);
 
     const formatDate = (iso) => {
@@ -32,7 +37,7 @@ const ConcertDetails = ({ concert }) => {
                                 <rect x="3" y="4" width="18" height="18" rx="2" /><line x1="16" y1="2" x2="16" y2="6" /><line x1="8" y1="2" x2="8" y2="6" /><line x1="3" y1="10" x2="21" y2="10" />
                             </svg>
                         </div>
-                        {formatDate(concert.date)}
+                        {formatDate(date)}
                     </div>
 
                     <div className="border-t border-slate-100" />
